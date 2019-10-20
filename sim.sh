0b15e7d70f39c567ffb4e8dd0e195f21ac7d9e35
Nathan Glover <nathan@glovers.id.au>
2019-10-20 15:07:58 +0800

    fixed sim

--- sim.sh
+++ sim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ask simulate -t "open space apps ground control"    -l "en-US"  --force-new-session
-ask simulate -t "change altitude to 5000"    -l "en-US"
-ask simulate -t "navigate to alpha"         -l "en-US"
+ask simulate -t "change altitude to 5000"           -l "en-US"
+ask simulate -t "navigate to alpha"                 -l "en-US"
